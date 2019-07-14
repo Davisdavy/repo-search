06dc628a9238961046e6af1b2851e6e5e0482882
Davisdavy <davisdvay96@gmail.com>
2019-07-14 10:44:09 +0300

    Add routing imports

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,5 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from '../profile/profile.component';
+import { HomeComponent } from '../home/home.component';
+import { RepositoryComponent } from '../repository/repository.component';
+import { PageErrorComponent} from '../page-error/page-error.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'repo', component: RepositoryComponent},
+  {path: '' , redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageErrorComponent}
+];
 
 @NgModule({
   declarations: [],
