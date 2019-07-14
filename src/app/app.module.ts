c9db1d88f0fb4195138735e3b8f0d356407efd9b
Davisdavy <davisdvay96@gmail.com>
2019-07-14 04:42:34 +0300

    Import required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileService } from './services/profile.service';
+import { HomeComponent } from './home/home.component';
+import { PageErrorComponent } from './page-error/page-error.component';
+import { RepositoryComponent } from './repository/repository.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: PageErrorComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent,
+    PageErrorComponent,
+    RepositoryComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.github.com/users/',
+  apiRepos: '/repos?access_token=227b075f172f9ba7715226bf57012060566ebdfa',
+  apiKey: '?access_token=227b075f172f9ba7715226bf57012060566ebdfa'
 };
 
 /*
