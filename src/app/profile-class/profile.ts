dd22e858b23aaa79dfb03496715e97f6a439f6fe
Davisdavy <davisdvay96@gmail.com>
2019-07-15 12:37:27 +0300

    Disable indentation alert

--- src/app/profile-class/profile.ts
+++ src/app/profile-class/profile.ts
@@ -1,7 +1,7 @@
 export class Profile {
 // tslint:disable-next-line: variable-name
 constructor(public avatar_url: string, public name: string, public url: string,
-            // tslint:disable-next-line: variable-name
+            // tslint:disable-next-line:variable-name
             public bio: string, public location: string, public public_repos: number,
             // tslint:disable-next-line: variable-name
             public followers: number, public following: number, public created_at: Date) {}
